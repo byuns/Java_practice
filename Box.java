afa41087957a032a4ffae2c307286e47692318c9
byuns <bsj0887@daum.net>
2022-01-18 22:19:21 +0900

    2022-01-18

---
+++ Box.java
@@ -0,0 +1,15 @@
+// Hello Ãâ·Â
+package Test;
+import java.util.*;
+
+public class Box {
+	public static void main(String[] args) {
+		
+		
+		
+		String str = "Hello";
+		
+		System.out.println(str);
+	}
+
+}
---
+++ MathRandom.java
@@ -0,0 +1,11 @@
+// 난수 생성 테스팅
+package Test;
+
+public class MathRandom {
+	public static void main(String[] args) {
+		int a = (int)(Math.random()*6); // 난수 생성 >>> 0~5
+		
+		System.out.println(a);
+	}
+
+}
