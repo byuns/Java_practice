1f0b3c06d48f93b8c793ceaa3b2bf1766b056b7c
byuns <bsj0887@daum.net>
2022-01-20 19:55:41 +0900

    2022-01-20

---
+++ ClassTest/C_05_MyMath.java
@@ -0,0 +1,11 @@
+package ClassTest;
+
+public class C_05_MyMath {
+	int square(int i) {
+		return i*i;
+	}
+	double square(double i) {
+		return i * i;
+	}
+
+}
---
+++ ClassTest/C_05_MyMathTest.java
@@ -0,0 +1,11 @@
+// 메소드 오버로딩
+package ClassTest;
+
+public class C_05_MyMathTest {
+	public static void main(String[] args) {
+		C_05_MyMath obj = new C_05_MyMath();
+		System.out.println(obj.square(10));
+		System.out.println(obj.square(3.14));
+	}
+
+}
