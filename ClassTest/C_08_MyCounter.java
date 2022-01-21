4e50b11bcfe433f61a1bdec05ffcf2db1f6e3a52
byuns <bsj0887@daum.net>
2022-01-21 16:01:31 +0900

    2022-01-21

---
+++ ClassTest/C_08_MyCounter.java
@@ -0,0 +1,10 @@
+package ClassTest;
+
+public class C_08_MyCounter {
+	int counter;
+	C_08_MyCounter(int value){
+		counter = value;
+	}
+
+}
+
---
+++ ClassTest/C_08_MyCounterTest.java
@@ -0,0 +1,13 @@
+package ClassTest;
+
+public class C_08_MyCounterTest {
+	public static void main(String[] args) {
+		C_08_MyCounter obj1 = new C_08_MyCounter(100);
+		C_08_MyCounter obj2 = new C_08_MyCounter(200);
+		
+		System.out.println("°´Ã¼ 1ÀÇ counter = " + obj1.counter);
+		System.out.println("°´Ã¼ 2ÀÇ counter = " + obj2.counter);
+
+	}
+
+}
